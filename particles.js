18b8c95a397cd69dfe7920b07c9d4762599e2183
Jefferson Lucas <jeffersonsombra70@gmail.com>
2025-02-16 15:03:40 -0300

    Add files via upload

---
+++ particles.js
@@ -0,0 +1,15 @@
+particlesJS("particles-js", {
+    "particles": {
+        "number": { "value": 120 },
+        "color": { "value": "#ff6600" },
+        "shape": { "type": "circle" },
+        "size": { "value": 3 },
+        "move": { "enable": true, "speed": 2 }
+    },
+    "interactivity": {
+        "events": {
+            "onhover": { "enable": true, "mode": "repulse" }
+        }
+    },
+    "retina_detect": true
+});
